8a48789788461e13f07b24a37501d10320848fcf
fajetin <kyuyto@gmail.com>
2022-10-29 02:57:39 +0200

    Adding list positions

--- js/showmenu.js
+++ js/showmenu.js
@@ -1,5 +1,22 @@
-function hidemenu(){
+function anulujhidemenu(){
     document.getElementById('addmenu').classList.remove("visiblemenu");
+    document.getElementById('nowyprodukt').value = "";
+    document.getElementById('nowyilosc').value = "";
+}
+
+function dodajhidemenu(){
+    document.getElementById('addmenu').classList.remove("visiblemenu");
+    
+    content = "<div class=\"itemtemplate\"><div class=\"detale\"><div class=\"cokupic\">";
+    content += document.getElementById('nowyprodukt').value;
+    content += "</div><div class=\"ilekupic\">";
+    content += document.getElementById('nowyilosc').value;
+    content += "</div></div><div class=\"delete\"><img class=\"deleteicon\" src=\"img/delete.png\"/></div></div>";
+    
+    document.getElementById('maincontent').innerHTML += content;
+    
+    document.getElementById('nowyprodukt').value = "";
+    document.getElementById('nowyilosc').value = "";
 }
 
 function showmenu(){
